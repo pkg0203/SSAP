82f623caa50cf6d0b148efe9c188797729da541e
pkg0203 <pkg0203@naver.com>
2024-05-20 12:25:59 +0900

    event 삭제 구현 완료

--- SSAP/events/urls.py
+++ SSAP/events/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    path('',EventAPIView.as_view())
+    path('',EventAPIView.as_view()),
+    path('<int:pk>/',EventDetailAPIView.as_view())
 ]
--- SSAP/events/views.py
+++ SSAP/events/views.py
@@ -1,29 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import *
 from .models import Event
-from .serializers import (
-    EventViewSerializer,
-    EventCreateSerializer
-    )
+from .serializers import EventViewSerializer, EventCreateSerializer
 import datetime
 from accounts.models import User
+
+
 # Create your views here.
 class EventAPIView(APIView):
     # 이번 달에 속한 이벤트만 가져오도록
-    def get(self,request):
-        #if request.user.is_authenticated:
+    def get(self, request):
+        # if request.user.is_authenticated:
         now = datetime.datetime.now()
-        events = Event.objects.filter(start_at__lte=now,end_at__gte=now)
-        serializer = EventViewSerializer(events,many=True)
-        return Response(serializer.data,status=HTTP_200_OK)
-    
-    def post(self,request):
+        events = Event.objects.filter(start_at__lte=now, end_at__gte=now)
+        serializer = EventViewSerializer(events, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
+
+    def post(self, request):
         # if request.user.is_superuser:
-        serializer=EventCreateSerializer(data=request.data)
+        serializer = EventCreateSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            #아직 로그인 구현 전
+            # 아직 로그인 구현 전
             serializer.save(user=User.objects.filter(pk=11)[0])
-            #serializer.save(user=request.user)
-            return Response(serializer.data,status=HTTP_201_CREATED)
+            # serializer.save(user=request.user)
+            return Response(serializer.data, status=HTTP_201_CREATED)
+
+
+class EventDetailAPIView(APIView):
+    def delete(self, request, pk):
+        # if request.user.is)superuser:
+        event = get_object_or_404(Event, pk=pk)
+        event.delete()
+        message = f"event pk : {pk} has been successfully deleted"
+        return Response({message}, status=HTTP_200_OK)
