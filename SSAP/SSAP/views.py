f1cf5dd2e3dcacca059ca4fc37eb75d5eb5cf4fe
pkg0203 <pkg0203@naver.com>
2024-05-20 20:05:04 +0900

    main_page 구현 거의 다 됨 but 어떤 기준으로 가져올지 회의 필요

--- SSAP/SSAP/views.py
+++ SSAP/SSAP/views.py
@@ -1,8 +1,21 @@
 from rest_framework.decorators import api_view
+from rest_framework.views import Response
+from django.db.models import Count
 from articles.models import Article
 from stories.models import Story
 
+ARTICLE_TO_GET = 5
+STORY_TO_GET = 5
+
 @api_view(["GET"])
 def index(request):
-    recs_articles = Article.objects.all()
-    recs_stories = Story.objects.all()
+    recs_articles = Article.objects.annotate(
+        count=Count("bookmarked"), 
+        like=Count("liked")
+    ).order_by("-count","-liked","-created_at")[:ARTICLE_TO_GET]
+    
+    recs_stories = Story.objects.annotate(
+        count=Count("bookmarked"), 
+        like=Count("liked")
+    ).order_by("-count","-liked","-created_at")[:STORY_TO_GET]
+    return Response({})
