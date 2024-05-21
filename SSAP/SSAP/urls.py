673af1b047f8b56c1669ac67878aa7d4215e1e17
pkg0203 <pkg0203@naver.com>
2024-05-21 21:47:00 +0900

    migrate 날리고 다시 생성 + frontend app 생성

--- SSAP/SSAP/urls.py
+++ SSAP/SSAP/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # connect to FE
-    #path("", admin.site.urls),
     path('ssap/',views.MainPageListView.as_view()),
     path("ssap/accounts/", include("dj_rest_auth.urls")),
     path("ssap/accounts/", include("dj_rest_auth.registration.urls")),
@@ -15,4 +13,5 @@ urlpatterns = [
     path("ssap/comments/", include("comments.urls")),
     path("ssap/events/", include("events.urls")),
     path("ssap/stories/", include("stories.urls")),
+    #path("", include("frontend.urls")),
 ]
