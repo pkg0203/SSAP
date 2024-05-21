a3e50b2f7a8efe8637f95fbe8d964dd4a0b7f1ff
pkg0203 <pkg0203@naver.com>
2024-05-21 12:41:02 +0900

    config.py가 gitignore가 안 돼서 복구

--- SSAP/SSAP/config.py
+++
@@ -1,2 +0,0 @@
-DJANGO_SECRET_KEY = "django-insecure-2g8jc^!@g5j^d%wjfhxv-uzpbas5ws1xs%wjdv(zw2=7p!$^l2"
-
--- SSAP/SSAP/settings.py
+++ SSAP/SSAP/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.DJANGO_SECRET_KEY
+SECRET_KEY = "django-insecure-2g8jc^!@g5j^d%wjfhxv-uzpbas5ws1xs%wjdv(zw2=7p!$^l2"
 SOCIAL_AUTH_GOOGLE_CLIENT_ID = ""
 SOCIAL_AUTH_GOOGLE_SECRET = ""
 STATE = ""
