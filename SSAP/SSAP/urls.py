90020430d020f8407fed725e795b930a4f69c5a7
pkg0203 <pkg0203@naver.com>
2024-05-21 20:39:00 +0900

    FE Url 분리

--- SSAP/SSAP/urls.py
+++ SSAP/SSAP/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # connect to FE
+    #path("", admin.site.urls),
     path('ssap/',views.MainPageListView.as_view()),
     path("ssap/accounts/", include("dj_rest_auth.urls")),
     path("ssap/accounts/", include("dj_rest_auth.registration.urls")),
