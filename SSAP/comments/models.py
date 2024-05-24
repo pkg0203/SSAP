d02f018f54f2feb021af7070e3b1d6b9a34a7918
sumina-codewell <sumina110423@gmail.com>
2024-05-24 20:47:42 +0900

    Modify models.py

--- SSAP/comments/models.py
+++ SSAP/comments/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.conf import settings
+from articles.models import Article
+from stories.models import Story
 
 
 # Create your models here.
 class Article_Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     comment_at = models.ForeignKey(
@@ -14,6 +17,7 @@ class Article_Comment(models.Model):
 
 
 class Story_Comment(models.Model):
+    story = models.ForeignKey(Story, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     comment_at = models.ForeignKey(
