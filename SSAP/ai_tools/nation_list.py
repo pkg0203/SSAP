e451cc50a82483a479e1ae07dce867d8d41bb157
pkg0203 <pkg0203@naver.com>
2024-05-27 21:02:30 +0900

    nation_list update

--- SSAP/ai_tools/nation_list.py
+++ SSAP/ai_tools/nation_list.py
@@ -1,4 +1,12 @@
 tar_nation_dict={
     "Korea" : "KO",
-    "Japan" : "JA"
+    "Japan" : "JA",
+    "China" : "ZH",
+    "USA" : "EN-US",
+    "Spain" : "ES",
+    "France" : "FR",
+    "Russia" : "RU",
+    "Ukraine" : "UK",
+    "Turkey" : "TR",
+    "Germany" : "DE"
 }
