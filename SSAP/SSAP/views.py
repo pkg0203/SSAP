849ccc7deb4c04da1083857123af04fb5a1ef2da
pkg0203 <pkg0203@naver.com>
2024-05-27 11:16:03 +0900

    python black

--- SSAP/SSAP/views.py
+++ SSAP/SSAP/views.py
@@ -11,31 +11,27 @@ from stories.serializers import StorySerializer
 ARTICLE_TO_GET = 5
 STORY_TO_GET = 5
 
+
 class MainPageListView(ListAPIView):
-    permission_classes=[AllowAny]
+    permission_classes = [AllowAny]
     serializer_class_Article = ArticleSerializer
     serializer_class_Story = StorySerializer
 
     def get_queryset_Article(self):
         return Article.objects.annotate(
-        count=Count("bookmarked"), 
-        like=Count("liked")
-    ).order_by("-count","-liked","-created_at")[:ARTICLE_TO_GET]
-    
+            count=Count("bookmarked"), like=Count("liked")
+        ).order_by("-count", "-liked", "-created_at")[:ARTICLE_TO_GET]
+
     def get_queryset_Story(self):
         return Story.objects.annotate(
-        count=Count("bookmarked"), 
-        like=Count("liked")
-    ).order_by("-count","-liked","-created_at")[:STORY_TO_GET]
+            count=Count("bookmarked"), like=Count("liked")
+        ).order_by("-count", "-liked", "-created_at")[:STORY_TO_GET]
 
     def list(self, request, *args, **kwargs):
-        is_api_call = request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+        is_api_call = request.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest"
         article = self.serializer_class_Article(self.get_queryset_Article(), many=True)
         story = self.serializer_class_Story(self.get_queryset_Story(), many=True)
         if is_api_call:
-            return Response({
-                "**Articles**": article.data,
-                "**Stories**": story.data
-            })
-        else :
-            return render(request,"index.html")
+            return Response({"**Articles**": article.data, "**Stories**": story.data})
+        else:
+            return render(request, "index.html")
