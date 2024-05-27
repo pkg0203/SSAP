e7c90e749fe327f2377ea5766395197c786d89a9
pkg0203 <pkg0203@naver.com>
2024-05-27 19:22:15 +0900

    상위 경로 import 추가

--- SSAP/ai_tools/translators.py
+++ SSAP/ai_tools/translators.py
@@ -1,9 +1,25 @@
 import deepl
+import sys
+import os
+
+# 현재 파일의 디렉토리 경로를 구합니다.
+current_path = os.path.dirname(__file__)
+
+# 상위 경로의 상대 경로를 구합니다.
+parent_path = os.path.join(current_path, "..")
+
+# sys.path에 상위 경로를 추가합니다.
+sys.path.append(parent_path)
+
+# 상위 경로의 파일을 import하기 위함
 from SSAP.config import DEEPL_SECRET_KEY
 
 TRANSLATOR = deepl.Translator(DEEPL_SECRET_KEY)
 
 
-def translate_text(text, target_lang="EN"):
+def translate_text(text, target_lang="EN-US"):
     result = TRANSLATOR.translate_text(text, target_lang=target_lang)
     return result.text
+
+
+print(translate_text("이거 맞냐?"))
