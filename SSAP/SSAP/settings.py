768f9de25651847ffdfa2bc6a02cb3ab77c9cc2d
pkg0203 <pkg0203@naver.com>
2024-05-28 20:38:12 +0900

    setting 커밋

--- SSAP/SSAP/settings.py
+++ SSAP/SSAP/settings.py
@@ -69,8 +69,8 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        #"rest_framework.authentication.SessionAuthentication",
-        "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
 
