d83b430f138e7971193b795484ede49621ee3ee3
pkg0203 <pkg0203@naver.com>
2024-05-28 20:25:23 +0900

    지원하는 언어는 번역 / 그렇지 않으면 Error message 생성

--- SSAP/ai_tools/translators.py
+++ SSAP/ai_tools/translators.py
@@ -1,6 +1,7 @@
 import deepl
 import sys
 import os
+from nation_list import tar_nation_dict
 
 # 현재 파일의 디렉토리 경로를 구합니다.
 current_path = os.path.dirname(__file__)
@@ -13,12 +14,18 @@ sys.path.append(parent_path)
 
 # 상위 경로의 파일을 import하기 위함
 from SSAP.config import DEEPL_SECRET_KEY
-from .nation_list import tar_nation_dict
 
 TRANSLATOR = deepl.Translator(DEEPL_SECRET_KEY)
 
 
-def translate_text(text, target_lang="EN-US"):
-    result = TRANSLATOR.translate_text(text, target_lang=target_lang)
-    return result.text
+def translate_text(text, target_lang):
+    # 본인 국가의 언어로 번역 시도
+    try: 
+        result = TRANSLATOR.translate_text(text, target_lang=tar_nation_dict[target_lang])
+        return result.text
+    # 지원하지 않는 언어일 때
+    except:
+        error_message="It's not supported Language. Following Languages are supported\n"+"---"*20 +"\n"+ str(tar_nation_dict.keys())[12:-2]
+        return error_message
+
 
