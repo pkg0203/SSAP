b5d098b9b2c4fe810ef6ca1ea259b92c08ad7d05
pkg0203 <pkg0203@naver.com>
2024-05-31 19:53:05 +0900

    프롬프트 변경

--- SSAP/ai_tools/chatgpt.py
+++ SSAP/ai_tools/chatgpt.py
@@ -15,36 +15,34 @@ sys.path.append(parent_path)
 # 상위 경로의 파일을 import하기 위함
 from SSAP.config import OPEN_AI_SECRET_KEY
 
-MAX_TOKEN = 40
+MAX_TOKEN = 5
 CLIENT = OpenAI(api_key=OPEN_AI_SECRET_KEY)
 
 
 def Korean_name(foreign_name):
     prompt_message = """
-            너는 외국인의 이름을 받아서 한국 이름으로 지어주는 작명가야.
-            외국 이름을 입력하면 그 발음을 알파벳으로 변환해서 
-            이름의 이니셜 자음과 모음 발음을 한글에서 발음이 비슷한 자음, 모음으로 바꿔서 
-            한국인스러운 이름으로 바꿔줘
-
-            아래의 2가지 규칙을 준수해야 해.
-            1. 3글자로 된 한글이름을 1개만 대답해야 돼
-            2. 이름만 답해
-            3. 아래의 예시를 참조해
-
-            answer_example= {
-                LeBron James : [류재민, 이재민],
-                Stephen Curry : 김성호,
-                Michael Jordan : 마재동,
-                Elon Musk : 이로먼,
-                Napoléon Bonaparte : 나보현,
-                尾田 栄一郎 : 오진우,
-                豐臣秀吉 : [도희수 , 도현우],
-                Elizabeth Windsor : 이리사
-                うずまき ナルト : 나건우,
-                Usain Bolt : 우세볼,
-                Mark Zuckerberg : 마주빈
-            }         
-        """
+역할:
+    - 너는 외국인의 이름을 받아서 한국 이름으로 바꿔주는 작명가야.
+작업:
+    - 외국 이름을 입력하면 발음을 한글 발음으로 변환해줘.
+    - 한글 발음을 기반으로 3글자로 된 한국 이름을 만들어줘.
+    - 이름만 답변으로 줘.
+규칙:
+    1. 3글자로 된 한글 이름을 1개만 답해.
+    2. 이름만 답해.
+예시:
+    - LeBron James : 이재민
+    - Stephen Curry : 김성호
+    - Michael Jordan : 마재동
+    - Elon Musk : 이로먼
+    - Napoléon Bonaparte : 나보현
+    - 尾田 栄一郎 : 오진우
+    - 豐臣秀吉 : 도현우
+    - Elizabeth Windsor : 이리사
+    - うずまき ナルト : 나건우
+    - Usain Bolt : 우세볼
+    - Mark Zuckerberg : 마주빈        
+"""
 
     completion = CLIENT.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -63,4 +61,4 @@ def Korean_name(foreign_name):
 
     return completion.choices[0].message.content
 
-print(Korean_name("Justin Bieber"))
+print(Korean_name("Aatrox"))
