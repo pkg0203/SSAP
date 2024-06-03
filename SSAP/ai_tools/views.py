d260dd7585ab3181f6fa3cb8ec44ea25276deee1
pkg0203 <pkg0203@naver.com>
2024-06-03 19:20:20 +0900

    ai view 작성 및 frontend app 삭제

--- SSAP/ai_tools/views.py
+++ SSAP/ai_tools/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .permissions import OnlyAuthenticated
+from .chatgpt import Korean_name
+from .translators import translate_text
 # Create your views here.
+class KoreanNameAPIView(APIView):
+    permission_classes=[OnlyAuthenticated]
+    def get(request,name):
+        message = Korean_name(name)
+        return Response({message})
+    
+class TranslateAPIView(APIView):
+    permission_classes=[OnlyAuthenticated]
+    def get(request,text):
+        message = translate_text(text)
+        return Response({message})
