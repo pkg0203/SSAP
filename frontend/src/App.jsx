ccf7c9e4377df43f06ef96c6390ed44d656a5367
kngslbm <kangseulbeom@gmail.com>
2024-06-03 16:34:48 +0900

    footer 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './componenets/header/Header'
+import Footer from './componenets/Footer'
 
 function App() {
 
   return (
     <div className="max-w-screen-2xl mx-auto">
       <Header />
+      <div className='min-h-[calc(100vh-136px)]'>
       <Outlet />
+      </div>
+      <Footer />
     </div>
   )
 }
