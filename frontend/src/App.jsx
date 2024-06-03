50b542158dd7952a9995b3383eef3a772bb5b0d3
pkg0203 <pkg0203@naver.com>
2024-06-03 09:16:07 +0900

    FE Navbar CSS 작업 중

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,51 @@
 import React from "react";
-import Router from "./shared/Router";
+import { jwtDecode } from 'jwt-decode'
+import { useState, useEffect } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import FetchUser from './FetchUser'
+
+import Navbar from './Components/Navbar/Navbar'
+import Article from "./pages/Article"
+import Story from "./pages/Story"
+import Community from "./pages/Community"
+import Profile from "./pages/Profile"
+import Main from "./pages/Main"
 
 function App() {
-  return <Router />;
+  const navigate = useNavigate()
+  const [userInfos, setUserInfos] = useState([{}])
+  const [authenticated, setAuthenticated] = useState(false)
+  const checkAuthentication = async () => {
+    try {
+        const isAuthenticated = await FetchUser()
+        setAuthenticated(isAuthenticated)
+    } catch (error) {
+        console.error('Error occurred while fetching user:', error)
+    }
+  }
+
+  useEffect(() => {
+      checkAuthentication()
+      if (!authenticated) {
+          navigate(window.location.pathname, { replace: true })
+      } else {
+          const token = localStorage.getItem('accessToken')
+          const decoded = jwtDecode(token)
+          setUserInfos(decoded)
+      }
+  }, [navigate, authenticated]) // navigate를 의존성 배열에 추가
+  return (
+    <>
+      <Navbar username={userInfos.username} />
+      <Routes>
+        <Route path="" element={<Main />} />
+        <Route path="article/" element={<Article />} />
+        <Route path="story/" element={<Story />} />
+        <Route path="community/" element={<Community />} />
+        <Route path="profile/" element={<Profile />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
