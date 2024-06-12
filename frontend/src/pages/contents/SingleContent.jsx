b5a3804f4cb50bd169b0f3d918b6e9e13c9c09f1
kngslbm <kangseulbeom@gmail.com>
2024-06-13 01:41:20 +0900

    article 내용 줄바꿈 적용

--- frontend/src/pages/contents/SingleContent.jsx
+++ frontend/src/pages/contents/SingleContent.jsx
@@ -7,15 +7,17 @@ const SingleContent = () => {
 
     return (
         <section className='min-h-dvh md:flex justify-center items-center md:bg-eggshell'>
-            <div className='bg-[#ffffff] p-8 md:my-[5rem] md:py-8 pb-8 md:rounded-xl'>
+            <div className='bg-[#ffffff] p-8 md:my-[1rem] md:py-8 pb-8 md:rounded-xl'>
                 <picture>
                     <img src={item.img} alt="" className='md:max-w-[90%] w-full md:h-[570px] md:rounded-xl md:mx-auto' />
                 </picture>
 
-                <div className='px-8'>
-                    <h1 className='text-4xl mt-12 text-secondary'>{item.title}</h1>
+                <div className='mb-12 px-8'>
+                    <h1 className='text-4xl mb-12 mt-12 text-secondary'>{item.title}</h1>
                     <article>
-                        <h2>{item.content}</h2>
+                        {item.content.split('\n').map((paragraph, index) => (
+                            <p key={index}>{paragraph}</p>
+                        ))}
                     </article>
                 </div>
 
