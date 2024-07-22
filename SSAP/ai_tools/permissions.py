7f7b3afe0799589e9a1364417ad382b1be05e936
pkg0203 <pkg0203@naver.com>
2024-07-23 01:44:32 +0900

    refactor : permission 통합

--- SSAP/ai_tools/permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import permissions
-
-class OnlyAuthenticated(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated
