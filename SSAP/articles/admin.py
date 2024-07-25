6c7b0bbc6b42490012d9c0632559fc8c04a1bf95
kngslbm <kangseulbeom@gmail.com>
2024-07-25 14:45:28 +0900

    etc : admin site에 model 추가 등록

--- SSAP/articles/admin.py
+++ SSAP/articles/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, ArticleLike, ArticleBookmark
 
 admin.site.register(Article)
+admin.site.register(ArticleLike)
+admin.site.register(ArticleBookmark)
