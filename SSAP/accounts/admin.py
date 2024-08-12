fac2f2507a9bdf2ed3b01e73b622fc82260dcbdd
pkg0203 <pkg0203@naver.com>
2024-08-12 21:10:48 +0900

    refactor : accounts 날리는 중

--- SSAP/accounts/admin.py
+++ SSAP/accounts/admin.py
@@ -1,15 +1,3 @@
 from django.contrib import admin
-from rest_framework_simplejwt.token_blacklist.admin import \
-    OutstandingTokenAdmin
-from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
 
-from .models import User
-
-
-class CustomOutstandingTokenAdmin(OutstandingTokenAdmin):
-    def has_delete_permission(self, *args, **kwargs):
-        return True
-
-
-admin.site.unregister(OutstandingToken)
-admin.site.register(User)
+# Register your models here.
